6ad501714af9b3e6143a665e6e33e7ddd78929a1
nazurinian <hikambinsaif@gmail.com>
2023-12-15 20:50:40 +0700

    perbaikan perpindahan halaman dr forgot password page ke login page (UDH di FIX)

--- app/src/main/java/com/submission/soilink/view/forgotpassword/ForgotPasswordActivity.kt
+++ app/src/main/java/com/submission/soilink/view/forgotpassword/ForgotPasswordActivity.kt
@@ -102,7 +102,7 @@ class ForgotPasswordActivity : AppCompatActivity() {
         }
 
         binding.loginPageButton.setOnClickListener {
-            val intent = Intent(this, RegisterActivity::class.java)
+            val intent = Intent(this, LoginActivity::class.java)
             intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
             startActivity(intent)
         }
