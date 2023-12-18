367d203acecfa0b1c54a606f4e44b55cb6b9bf37
nazurinian <hikambinsaif@gmail.com>
2023-12-19 03:12:44 +0700

    konfigurasi awal halaman detail history (by hikam)

--- app/src/main/java/com/submission/soilink/view/detailhistory/DetailHistoryActivity.kt
+++ app/src/main/java/com/submission/soilink/view/detailhistory/DetailHistoryActivity.kt
@@ -2,12 +2,41 @@ package com.submission.soilink.view.detailhistory
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import com.submission.soilink.R
+import com.submission.soilink.databinding.ActivityDetailHistoryBinding
+import com.submission.soilink.util.showToast
 
 class DetailHistoryActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityDetailHistoryBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detail_history)
+        binding = ActivityDetailHistoryBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupAction()
+    }
+
+    private fun setupAction() {
+        val toolbar = binding.topAppBar
+        setSupportActionBar(toolbar)
+        supportActionBar?.apply {
+            setDisplayHomeAsUpEnabled(true)
+            setDisplayShowHomeEnabled(true)
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                onBackPressedDispatcher.onBackPressed()
+                true
+            }
+
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 
     companion object {
